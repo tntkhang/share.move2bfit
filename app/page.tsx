2a598a8a2f9dddb2d81e5e8327e6c8e8be290ac5
Khang Tran <tntkhang@gmail.com>
2024-12-09 18:32:30 +0700

    Update navigation to store

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const isIOS = /iPad|iPhone|iPod/.test(userAgent);
+    const isAndroid = /Android/.test(userAgent);
+
+    if (isIOS) {
+      window.location.href = 'https://apps.apple.com/us/app/facebook/id284882215';
+    } else if (isAndroid) {
+      window.location.href = 'https://play.google.com/store/apps/details?id=com.facebook.katana';
+    }
+  }, []);
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
